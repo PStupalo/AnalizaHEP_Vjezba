f562e299067aad33a23fa4256176918371253f79
PStupalo <pstupalo@pmfst.hr>
2024-02-15 12:19:53 +0100

    commit

---
+++ AnalizaHEP_Vjezba/Kolokvij_1/Zadatak_1/Generator.cpp
@@ -0,0 +1,38 @@
+#include "Generator.h"
+
+Generator::Generator(){
+
+}
+
+void Generator::GeneratePDF(){
+	srand((unsigned)1);
+	i = 0;
+	canvas = new TCanvas ();
+	hist = new TH1F ("GeneratePDF", "GeneratePDF", 48, -3., 9.); //48 binova, u rangu [-3,9]
+	while (i < 1E5){ //generiramo 10^5 vrijednosti
+		rn1 = -3 + 12 * (float)rand () / RAND_MAX;
+		f = (-rn1 * rn1 + 6 * rn1 + 27) / 36.; //-(x+3)(x-9)=-(x^2-6x-27)=-x^2+6x+27
+		rn2 = -3 + 12 * (float)rand () /RAND_MAX;
+		if (rn2 < f){
+			hist -> Fill (rn1);
+			i++;
+		}
+	}
+	gPad -> SetLeftMargin (0.20);
+	gPad -> SetBottomMargin (0.20);
+	canvas -> SetCanvasSize (800, 800);
+	canvas -> SetTitle ("Histogram generiranih vrijednosti");
+	hist->SetFillColor(kRed);
+  	hist->SetLineColor(kRed);
+	hist -> Draw ("HIST");
+	hist -> GetXaxis () -> SetTitle ("Generated value");
+	hist -> GetYaxis () -> SetTitle ("Events / 0.25");
+	hist -> GetXaxis () -> SetLabelSize (0.03);
+	hist -> GetYaxis () -> SetLabelSize (0.03);
+	
+	canvas -> Print ("Zadatak_1.pdf");
+	canvas -> Print ("Zadatak_1.png");
+	canvas -> Print ("Zadatak_1.root");
+
+}
+
