63abe44e16a76213521c680ba2b57b5f68252514
PStupalo <pstupalo@pmfst.hr>
2024-08-28 02:25:41 +0200

    vj6

---
+++ Vjezbe/Vjezbe_6/analyze.cpp
@@ -0,0 +1,22 @@
+// C++
+#include <iostream>
+#include <fstream>
+
+#include <Analyzer.h>
+
+using namespace std;
+
+int main()
+{
+  // Declare Analyzer
+  Analyzer *analyzer;
+
+  analyzer = new Analyzer();
+
+  analyzer->PlotHistogram("/home/public/data/ggH125/ZZ4lAnalysis.root");
+  analyzer->PlotMass();
+
+  delete analyzer;
+
+  return 0;
+}
